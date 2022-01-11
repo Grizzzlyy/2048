c6877d5b6b1aa894c5c20b2021a07e1232ab0854
Софья Харитонова <haritonova.sa@edu.spbstu.ru>
2022-01-11 21:01:30 +0300

    Ассемблерная вставка для вниз

--- Coursework/2048.c
+++ Coursework/2048.c
@@ -315,8 +315,22 @@ void swipeFieldAndChangeScore(player_t* player, int boardSize, enum action actio
                 //Если они равны, то склеиваем,увеличиваем счет и подтягиваем остальные плитки
                 if (player->board[j][i] == player->board[j - 1][i] && player->board[j][i] != 0)
                 {
-                    player->board[j][i] *= 2;
-                    player->score += player->board[j][i];
+                    //player->board[j][i] *= 2;
+                    //player->score += player->board[j][i];
+                    int a = player->board[j][i] , b = player->score;
+                    __asm
+                    {
+                        mov eax, a
+                        mov ebx, b
+                        mov ecx,2
+                        mul ecx
+                        add ebx, eax
+                        mov a, eax
+                        mov b,ebx
+                    }
+                    player->score = b;
+                    player->board[j][i] = a;
+
                     int k;
                     for (k = j - 1; k >= 1; k--)
                     {
