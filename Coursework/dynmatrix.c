7c3952390b93a8614fe14104a4b7c61738e0d0f5
AlenaTDE <poleshchuk.alena@icloud.com>
2022-01-17 19:36:58 +0300

    Исправила циклы в dynamatrix.c

--- Coursework/dynmatrix.c
+++ Coursework/dynmatrix.c
@@ -32,14 +32,14 @@ void deleteMatrix(unsigned short** matrix, unsigned short size)
 //Копирует sourceMatrix в destinationMatrix
 void copyMatrix(unsigned short** sMatrix, unsigned short** dMatrix, unsigned short size)
 {
-    for (unsigned short i = 0; i < size; i += 4)
+    for (unsigned short i = 0; i < size; i ++)
     {
         for (unsigned short j = 0; j < size; j += 4)
         {
             dMatrix[i][j] = sMatrix[i][j];
-            dMatrix[i + 1][j + 1] = sMatrix[i + 1][j + 1];
-            dMatrix[i + 2][j + 2] = sMatrix[i + 2][j + 2];
-            dMatrix[i + 3][j + 3] = sMatrix[i + 3][j + 3];
+            dMatrix[i][j + 1] = sMatrix[i][j + 1];
+            dMatrix[i][j + 2] = sMatrix[i][j + 2];
+            dMatrix[i][j + 3] = sMatrix[i][j + 3];
         }
     }
 }
@@ -47,11 +47,11 @@ void copyMatrix(unsigned short** sMatrix, unsigned short** dMatrix, unsigned sho
 //Если матрицы разные, возвращает 1, если одинаковые, то 0
 bool cmpMatrix(unsigned short** matrix1, unsigned short** matrix2, int size)
 {
-    for (int i = 0; i < size; i += 2)
+    for (int i = 0; i < size; i ++)
     {
         for (int j = 0; j < size; j += 2)
         {
-            if (matrix1[i][j] != matrix2[i][j] || matrix1[i+1][j+1] != matrix2[i+1][j+1] )
+            if (matrix1[i][j] != matrix2[i][j] || matrix1[i][j+1] != matrix2[i][j+1] )
                 return 1;
         }
     }
@@ -60,11 +60,11 @@ bool cmpMatrix(unsigned short** matrix1, unsigned short** matrix2, int size)
 
 void printMatrix(unsigned short** matrix, unsigned short size)
 {
-    for (int i = 0; i < size; i += 4)
+    for (int i = 0; i < size; i ++)
     {
         for (int j = 0; j < size; j += 4)
         {
-            printf("%hu %hu %hu %hu ", matrix[i][j], matrix[i + 1][j + 1], matrix[i + 2][j + 2], matrix[i + 3][j + 3]);
+            printf("%hu %hu %hu %hu ", matrix[i][j], matrix[i][j + 1], matrix[i][j + 2], matrix[i][j + 3]);
         }
         printf("\n");
     }
