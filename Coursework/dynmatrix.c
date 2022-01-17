baa5485aae67cfe3ae0aaeecaacfc6cb12d1dc15
AlenaTDE <poleshchuk.alena@icloud.com>
2022-01-17 19:45:15 +0300

    Вывод матрицы исправления

--- Coursework/dynmatrix.c
+++ Coursework/dynmatrix.c
@@ -60,7 +60,7 @@ bool cmpMatrix(unsigned short** matrix1, unsigned short** matrix2, int size)
 
 void printMatrix(unsigned short** matrix, unsigned short size)
 {
-    for (int i = 0; i < size; i += 4)
+    for (int i = 0; i < size; i++)
     {
         for (int j = 0; j < size; j += 4)
         {
